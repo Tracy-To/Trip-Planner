f1cbc582727648a2a5fd11e40f7ad85d4569facc
Tracy To <tracyto101@gmail.com>
2024-04-12 19:50:05 -0400

    connected Express to Mongo

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const mongoose = require('mongoose')
+const mongoURI = process.env.MONGO_URI
 const PORT = process.env.PORT
 
+// Controllers
 const tripsController = require('./controllers/trips.js')
 app.use('/trips', tripsController)
 
+// Connect to Mongo
+mongoose.connect(mongoURI)
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + 'error with mongo connection'))
+db.on('connected', () => console.log('mongo is connected'))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+// Run Server
 app.listen(PORT, () => {
   console.log(`(Listening on Port ${PORT}) Server is standing by, at your service.`)
 })
